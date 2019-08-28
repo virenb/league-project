3645d3b21bfccae123218bf1010ca9b7ed3aa3cf
Viren Bhagat <virenb@gmail.com>
2019-08-28 15:47:47 -0400

    Added user

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,15 +1,23 @@
 const Team = require('./models/Team');
+const User = require('./models/User');
 
 const resolvers = {
 	Query: {
-		teams: () => Team.find()
+		teams: () => Team.find(),
+		users: () => User.find()
 	},
 	Mutation: {
 		createTeam: async (_, { name }) => {
 			const team = new Team({ name });
 			await team.save();
 			return team;
-		}
+		},
+		register: async (_, { email, name, password }) => {
+			const user = new User({ email, name, password });
+			await user.save();
+			return user;
+		},
+		login: () => {}
 	}
 };
 
--- server/typeDefs.js
+++ server/typeDefs.js
@@ -3,6 +3,7 @@ const gql = require('apollo-server-express').gql;
 const typeDefs = gql`
 	type Query {
 		teams: [Team!]!
+		users: [User!]!
 	}
 
 	type Team {
@@ -10,8 +11,22 @@ const typeDefs = gql`
 		name: String!
 	}
 
+	type User {
+		id: ID!
+		name: String!
+		email: String!
+		password: String!
+	}
+
 	type Mutation {
 		createTeam(name: String!): Team!
+		register(email: String!, name: String!, password: String!): User!
+		login(email: String!, password: String!): LoginResponse!
+	}
+
+	type LoginResponse {
+		token: String
+		user: User
 	}
 `;
 
